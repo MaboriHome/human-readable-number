dd52a00baaa1d0fdead1776b3a52f13a6517bfe5
Алексей Макеев <117520389+MaboriHome@users.noreply.github.com>
2023-01-11 20:56:27 +0300

    feat: update func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-module.exports = function toReadable (number) {
-    const unit = ['', 'one ', 'two ', 'three ', 'four ',
-    'five ', 'six ', 'seven ', 'eight ', 'nine ',
-    'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ',
-    'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
-  const decade = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty',
-    'seventy', 'eighty', 'ninety', 'one hundred'];
+module.exports = function toReadable(number) {
 
-  const numStr = number.toString().split('').reverse();
+  const unit = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const decade = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'one hundred'];
+
+  let numStr = number.toString().split('').reverse();
 
   if (number === 0) {
     return 'zero';
@@ -16,14 +13,23 @@ module.exports = function toReadable (number) {
     return unit[number];
   }
 
-  if (number % 10 === 0 && number !== 10 && number < 101) {
-    return decade[number / 10 + 1]
+  if (number % 10 === 0 && number < 101) {
+    return decade[number / 10]
   }
+
 
   if (number > 20) {
 
     let res = '';
     let i = 0;
+
+    if (numStr.length === 3 && number % 100 < 20) {
+
+      res = unit[Math.floor(number / 100)] + ' hundred ' + res;
+      res = res + unit[number % 100];
+
+      return res.trim();
+    }
 
     while (i < numStr.length) {
 
@@ -34,10 +40,10 @@ module.exports = function toReadable (number) {
         res = decade[numStr[i]] + ' ' + res;
       }
       if (i === 2) {
-        res = unit[numStr[i]] + 'hundred ' + res;
+        res = unit[numStr[i]] + ' hundred ' + res;
       }
       i++;
     }
-    return res;
-  }  
+    return res.trim();
+  }
 }
